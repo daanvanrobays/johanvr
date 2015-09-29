8a391dbda0fe27a0adc1d47c4e0012d961e428d0
Daan Van Robays <vanrobays.daan@hotmail.com>
2015-09-29 15:27:13 +0200

    Pages and controllers

---
+++ JohanVanRobays/Controllers/StamboomController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JohanVanRobays.Controllers
+{
+    public class StamboomController : Controller
+    {
+        // GET: Stamboom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Stamboom/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Stamboom/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stamboom/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stamboom/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Stamboom/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Stamboom/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Stamboom/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ JohanVanRobays/Controllers/VesaliusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JohanVanRobays.Controllers
+{
+    public class VesaliusController : Controller
+    {
+        // GET: Vesalius
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
