8b0671db4db399662ce8d93001e5b206b3c26da5
Daan Van Robays <vanrobays.daan@hotmail.com>
2015-10-23 13:25:50 +0200

    Added navbar autohide plugin
    added hobbyist index

---
+++ JohanVanRobays/Controllers/HobbyistController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JohanVanRobays.Controllers
+{
+    public class HobbyistController : Controller
+    {
+        // GET: Hobbyist
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
